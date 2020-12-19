224b679457dc412291c4d35fb6a41ce02679e9d4
dbqudgh <60841247+dbqudgh@users.noreply.github.com>
2020-12-20 02:28:45 +0900

    수정 완료

--- server/models/modelsOrderCoin.js
+++ server/models/modelsOrderCoin.js
@@ -1,107 +1,232 @@
 const request = require('request')
 const {
-    v4: uuidv4
+	v4: uuidv4
 } = require("uuid");
 const crypto = require('crypto')
 const sign = require('jsonwebtoken').sign
 const queryEncode = require("querystring").encode
 
 const {
-    access_key,
-    secret_key,
-    server_url
+	access_key,
+	secret_key,
+	server_url
 } = require("../config/config");
 
 
 
 function orderCoin(market, volume, price, side, cb) {
 
-    const body = {
-        market: market,
-        side: side,
-        volume: volume,
-        price: price,
-        ord_type: 'limit'
-    }
+	let ord_type = 'limit'
 
-    //8자리 까지
+	if (volume === null) {
+		ord_type = 'price'
+	}
 
 
-    const query = queryEncode(body)
+	if (price === null) {
 
-    const hash = crypto.createHash('sha512')
-    const queryHash = hash.update(query, 'utf-8').digest('hex')
+		ord_type = 'market'
 
-    const payload = {
-        access_key: access_key,
-        nonce: uuidv4(),
-        query_hash: queryHash,
-        query_hash_alg: 'SHA512',
-    }
+	}
 
-    const token = sign(payload, secret_key)
+	const body = {
+		market: market,
+		side: side,
+		volume: volume,
+		price: price,
+		ord_type: ord_type
+	}
 
-    const options = {
-        method: "POST",
-        url: server_url + "/v1/orders",
-        headers: {
-            Authorization: `Bearer ${token}`
-        },
-        json: body
-    }
+	//8자리 까지
 
-    request(options, (error, response, body) => {
-        if (error) {
 
-            console.log(error)
-            cb(JSON.stringify(error))
+	const query = queryEncode(body)
 
-        }else if (body.error) {
+	const hash = crypto.createHash('sha512')
+	const queryHash = hash.update(query, 'utf-8').digest('hex')
 
-            console.log(body.error)
-            cb(JSON.stringify(body.error))
+	const payload = {
+		access_key: access_key,
+		nonce: uuidv4(),
+		query_hash: queryHash,
+		query_hash_alg: 'SHA512',
+	}
 
-        }else{
-            cb(null,JSON.stringify(body))
-        }
-        // const state = body.state
-        // const uuid = body.uuid
-        // if (state === 'wait') {
-        //     cb(state)
-        //     const body = {
-        //         uuid: uuid
-        //     }
+	const token = sign(payload, secret_key)
 
-        //     const query = queryEncode(body)
+	const options = {
+		method: "POST",
+		url: server_url + "/v1/orders",
+		headers: {
+			Authorization: `Bearer ${token}`
+		},
+		json: body
+	}
 
-        //     const hash = crypto.createHash('sha512')
-        //     const queryHash = hash.update(query, 'utf-8').digest('hex')
+	request(options, (error, response, body) => {
 
-        //     const payload = {
-        //         access_key: access_key,
-        //         nonce: uuidv4(),
-        //         query_hash: queryHash,
-        //         query_hash_alg: 'SHA512',
-        //     }
+		const state = body.state
+		const uuid = body.uuid
 
-        //     const token = sign(payload, secret_key)
+		if (error) {
 
-        //     const options = {
-        //         method: "DELETE",
-        //         url: server_url + "/v1/order?" + query,
-        //         headers: {
-        //             Authorization: `Bearer ${token}`
-        //         },
-        //         json: body
-        //     }
+			console.log(error)
+			cb(JSON.stringify(error))
 
-        //     request(options, (error, response, body) => {
-        //         if (error) throw new Error(error)
-        //         console.log(body)
-        //     })
-        // }
-    })
+		} else if (body.error) {
+
+			console.log(body.error)
+			cb(JSON.stringify(body.error))
+
+		} else {
+			cb(null, JSON.stringify(body))
+		}
+
+
+
+
+
+	})
 }
 
 
 module.exports = orderCoin
+
+
+
+
+
+
+// const request = require('request')
+// const {
+// 	v4: uuidv4
+// } = require("uuid");
+// const crypto = require('crypto')
+// const sign = require('jsonwebtoken').sign
+// const queryEncode = require("querystring").encode
+
+// const {
+// 	access_key,
+// 	secret_key,
+// 	server_url
+// } = require("../config/config");
+
+
+
+// function orderCoin(market, volume, price, side, cb) {
+
+// 	let ord_type = 'limit'
+
+// 	if (volume === null) {
+// 		ord_type = 'price'
+// 	}
+
+
+// 	if (price === null) {
+
+// 		ord_type = 'market'
+
+// 	}
+
+// 	const body = {
+// 		market: market,
+// 		side: side,
+// 		volume: volume,
+// 		price: price,
+// 		ord_type: ord_type
+// 	}
+
+// 	//8자리 까지
+
+
+// 	const query = queryEncode(body)
+
+// 	const hash = crypto.createHash('sha512')
+// 	const queryHash = hash.update(query, 'utf-8').digest('hex')
+
+// 	const payload = {
+// 		access_key: access_key,
+// 		nonce: uuidv4(),
+// 		query_hash: queryHash,
+// 		query_hash_alg: 'SHA512',
+// 	}
+
+// 	const token = sign(payload, secret_key)
+
+// 	const options = {
+// 		method: "POST",
+// 		url: server_url + "/v1/orders",
+// 		headers: {
+// 			Authorization: `Bearer ${token}`
+// 		},
+// 		json: body
+// 	}
+
+// 	request(options, (error, response, body) => {
+
+// 		const state = body.state
+// 		const uuid = body.uuid
+
+// 		if (error) {
+
+// 			console.log(error)
+// 			cb(JSON.stringify(error))
+
+// 		} else if (body.error) {
+
+// 			console.log(body.error)
+// 			cb(JSON.stringify(body.error))
+
+// 		} else if (state === 'wait') {
+
+// 			const body = {
+// 				uuid: uuid
+// 			}
+
+// 			const query = queryEncode(body)
+
+// 			const hash = crypto.createHash('sha512')
+// 			const queryHash = hash.update(query, 'utf-8').digest('hex')
+
+// 			const payload = {
+// 				access_key: access_key,
+// 				nonce: uuidv4(),
+// 				query_hash: queryHash,
+// 				query_hash_alg: 'SHA512',
+// 			}
+
+// 			const token = sign(payload, secret_key)
+
+// 			const options = {
+// 				method: "DELETE",
+// 				url: server_url + "/v1/order?" + query,
+// 				headers: {
+// 					Authorization: `Bearer ${token}`
+// 				},
+// 				json: body
+// 			}
+
+// 			request(options, (error, response, body) => {
+// 				if (error) cb(JSON.stringify(error))
+// 				if (body) {
+// 					if (body.error) {
+// 						cb(JSON.stringify(body.error))
+// 					}
+// 					cb(`매수주문 취소합니다`)
+// 				}
+// 			})
+// 		} else {
+
+// 			cb(null, JSON.stringify(body))
+
+// 		}
+
+
+
+
+
+// 	})
+// }
+
+
+// module.exports = orderCoin
--- server/test2.js
+++ server/test2.js
@@ -6,155 +6,145 @@ const orderCoin = require("./models/modelsOrderCoin");
 
 // orderCoin(market,volume,price,side)
 let TRADS = {};
-
 let MY_ACCOUNT = {
-    market: null
+	market: null
 };
-
-
 let BYCOIN = false;
-
-
 //거래할 자금 만원으로 테스트
 const PRICE = 10000;
 
 function tradeServerConnect(codes, cb) {
 
-    var ws = new WebSocket('wss://api.upbit.com/websocket/v1');
+	var ws = new WebSocket('wss://api.upbit.com/websocket/v1');
 
-    ws.on('open', () => {
-        console.log('trade websocket is connected');
-        sendTelegramMessage('trade websocket is connected')
-        ws.send(`[{"ticket":"test"},{"type":"ticker","codes":[${codes}]}]`);
-    })
+	ws.on('open', () => {
+		console.log('trade websocket is connected');
+		sendTelegramMessage('trade websocket is connected')
+		ws.send(`[{"ticket":"test"},{"type":"ticker","codes":[${codes}]}]`);
+	})
 
-    ws.on('close', () => {
-        console.log('trade websocket is closed');
-        sendTelegramMessage('trade websocket is closed trade 재접속');
-        tradeServerConnect(codes)
-    })
+	ws.on('close', () => {
+		console.log('trade websocket is closed');
+		sendTelegramMessage('trade websocket is closed trade 재접속');
+		tradeServerConnect(codes)
+	})
 
-    ws.on('message', (data) => {
+	ws.on('message', (data) => {
 
-        try {
+		try {
 
-            const str = data.toString('utf-8')
-            const parseData = JSON.parse(str)
-            const marketName = parseData.code
+			const str = data.toString('utf-8')
+			const parseData = JSON.parse(str)
+			const marketName = parseData.code
 
-            if (BYCOIN && MY_ACCOUNT.market === marketName) { //내잔고랑 market 내임 있다면
+			if (BYCOIN && MY_ACCOUNT.market === marketName) { //내잔고랑 market 내임 있다면
 
-                const newTradePrice = parseData.trade_price;
-                const myPrice = MY_ACCOUNT.price;
+				const newTradePrice = parseData.trade_price;
+				const myPrice = MY_ACCOUNT.price;
 
+				const increase = getTheRateOfIncrease(newTradePrice, myPrice);
+				let text;
 
-                const increase = getTheRateOfIncrease(newTradePrice, myPrice);
-                let text;
+				increase > 0 ? text = "증가:" : text = "감소";
 
-                increase > 0 ? text = "증가:" : text = "감소";
+				const volume = (PRICE / newTradePrice).toFixed(8)
 
-                const volume = (PRICE / newTradePrice).toFixed(8)
-                const changePrice = (increase*PRICE)
+				console.log(`${text}${increase}`)
 
-                console.log(text,increase,"%")
+				if (increase < -1 || increase > 0.3) {
+					orderCoin(marketName, volume, null, 'ask', (error, body) => {
+						if (error) {
+							sendTelegramMessage(`${error}`)
+						} else if (body) {
 
-                if (increase < -1 || increase > 0.3) {
-                    sendTelegramMessage(`이득본가격:${changePrice}원`)
-                    BYCOIN = false;
+							if (body.error) {
 
-                    // orderCoin(marketName, volume, PRICE, 'ask', (error, body) => {
-                    //     if (error) {
-                    //         sendTelegramMessage(`${error}`)
-                    //     } else if (body) {
-                    //         sendTelegramMessage(`${text}:${changePrice-PRICE}`)
-                    //         BYCOIN = false;
-                    //     }
-                    // })
+							} else {
 
-                }
+								const increasePrice = ((increase / 100) * PRICE).toFixed(0)
+								console.log(`이득본가격:${increasePrice}원`)
+								sendTelegramMessage(`이득본가격:${increasePrice}원`)
 
-            } else if (TRADS[`${marketName}`]) {
+								MY_ACCOUNT = {
+									market: null
+								};
 
-                const newTradePrice = parseData.trade_price;
-                const oldTradePrice = TRADS[`${marketName}`].trade_price;
+								BYCOIN = false;
 
-                const increase = getTheRateOfIncrease(newTradePrice, oldTradePrice)
-                const volume = (PRICE / newTradePrice).toFixed(8);
+							}
 
-                //코인 구매 조건
-                if (increase > 2 && BYCOIN === false) {
-                    BYCOIN = true;
-                    const textData = `코인: ${marketName} 구매`
+							
+						}
+					})
 
-                                MY_ACCOUNT = {
+				}
 
-                                    market: marketName,
-                                    price: newTradePrice
-                                }
+			} else if (TRADS[`${marketName}`]) {
 
-                                sendTelegramMessage(textData)
+				const newTradePrice = parseData.trade_price;
+				const oldTradePrice = TRADS[`${marketName}`].trade_price;
 
+				const increase = getTheRateOfIncrease(newTradePrice, oldTradePrice)
+				const volume = (PRICE / newTradePrice).toFixed(8);
 
-                    // orderCoin(market,volume,price,side)
-                    // orderCoin(marketName, volume, newTradePrice, 'bid', (error, body) => {
+				//코인 구매 조건
+				if ((increase > 4 ) && BYCOIN === false) {
 
-                    //     if (error) {
+					BYCOIN = true;
+					// orderCoin(market,volume,price,side)
+					orderCoin(marketName, null, PRICE, 'bid', (error, body) => {
+						if (error) {
+							sendTelegramMessage(`${error}`)
+							BYCOIN = false;
 
-                    //         sendTelegramMessage(`${error}`)
-                    //         BYCOIN = false;
+						} else if (body) {
 
-                    //     } else if (body) {
+							if (body.error) {
 
-                    //         if (body.error) {
+								sendTelegramMessage(body.message)
 
-                    //             sendTelegramMessage(body.message)
+							} else {
 
-                    //         } else {
+								const textData = `코인: ${marketName} 구매`
+								console.log(textData)
 
-                    //             const textData = `코인: ${marketName} 구매`
+								MY_ACCOUNT = {
+									market: marketName,
+									price: newTradePrice
+								}
 
-                    //             MY_ACCOUNT = {
+								sendTelegramMessage(textData)
+								//구매했다는 가상을 새우고 그 데이터가 변화했다면 ? 비교	
+							}
 
-                    //                 market: marketName,
-                    //                 price: newTradePrice
-                    //             }
-
-                    //             sendTelegramMessage(textData)
-
-
-                    //             //구매했다는 가상을 새우고 그 데이터가 변화했다면 ? 비교	
-
-                    //         }
-
-
-                    //     } else {
-                    //         BYCOIN = false
-                    //     }
+						} else {
+							BYCOIN = false
+						}
 
 
-                    // })
-                }
+					})
+				}
 
-            }
+			}
 
-            TRADS[`${marketName}`] = parseData
+			TRADS[`${marketName}`] = parseData
 
 
-        } catch (e) {
-            sendTelegramMessage("서버에러");
-            console.log(e)
-        }
-    })
+		} catch (e) {
+			sendTelegramMessage("서버에러");
+			console.log(e)
+		}
+	})
 }
 
 function getTheRateOfIncrease(newData, oldData) {
-    const increase = (newData - oldData) / oldData * 100;
-    return increase
+	const increase = (newData - oldData) / oldData * 100;
+	return increase
 }
 
 async function main() {
-    const krwMarketList = await getAllKrwMarketList(); // 모든 마켓 정보 가저옴
-    tradeServerConnect(krwMarketList)
+	const krwMarketList = await getAllKrwMarketList(); // 모든 마켓 정보 가저옴
+	tradeServerConnect(krwMarketList)
 }
 
 main()
